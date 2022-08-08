6eea51635612c293d52cf994aba1c3244b873cec
JasperMyles <ograhjoshua@gmail.com>
2022-08-08 07:19:18 -0700

    Added my new printf

--- _printf.c
+++ _printf.c
@@ -1,53 +1,80 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _printf - prints all request
- * @format: format used
- *
- * Return: characters printed
+ * specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ * Return: pointer to printf function or 0
+ */
+static int (*specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t point[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
+
+	for (i = 0; point[i].t != NULL; i++)
+	{
+		if (*(point[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (point[i].f);
+}
+
+/**
+ * _printf - prints anything
+ * @format: list of argument types
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int result = 0;
-	va_list args;
+	unsigned int i = 0, result = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
 	while (format[i])
 	{
-		while (format[i] == '%')
-		{
-			switch (format[i + 1])
-			{
-				case 'c':
-					result += print_c(args);
-					i += 2;
-					break;
-				case 's':
-					result += print_s(args);
-					i += 2;
-					break;
-				case '%':
-					_putchar('%');
-					result++;
-					i += 2;
-					break;
-				default:
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					i += 2;
-			}
-		}
-
-		if (format[i])
+		for (; format[i] != '%' && format[i]; i++)
 		{
 			_putchar(format[i]);
 			result++;
 		}
-
-		i++;
+		if (!format[i])
+			return (result);
+		f = specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			result += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		result++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
 	}
-	va_end(args);
-
+	va_end(valist);
 	return (result);
 }
